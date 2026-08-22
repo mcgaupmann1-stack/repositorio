cc503b35b27b534f5530c83d4667820d1dd16694
mcgaupmann1-stack <mcgaupmann1@gmail.com>
2026-08-22 15:17:07 -0300

    Create m4_consultas_negocio.sql for sales queries
    Added multiple SQL queries for sales analysis including monthly summary, product ranking, repeat customers, and average comparison.

---
+++ modulo4/m4_consultas_negocio.sql
@@ -0,0 +1,62 @@
+SELECT * FROM ventas;
+
+--Consulta 1 — Resumen ejecutivo mensual 
+
+SELECT 
+    MONTH(fecha_venta) AS MES,
+    SUM(cantidad * precio_unitario) AS TOTAL_FACTURADO
+FROM 
+    ventas
+GROUP BY 
+    MONTH(fecha_venta);
+
+--Consulta 2 — Ranking de productos
+
+SELECT TOP 5
+    id_producto,
+    SUM(cantidad) AS UNIDADES_VENDIDAS,
+    SUM(cantidad * precio_unitario) AS TOTAL_FACTURADO
+FROM 
+    ventas
+GROUP BY 
+    id_producto
+ORDER BY 
+    TOTAL_FACTURADO DESC;
+
+--Consulta 3 — Clientes recurrentes
+
+SELECT 
+    id_cliente,
+    COUNT(*) AS CANTIDAD_PEDIDOS,
+    SUM(cantidad * precio_unitario) AS TOTAL_FACTURADO
+FROM 
+    ventas
+GROUP BY id_cliente
+HAVING COUNT(*) > 1
+
+
+--Consulta 4 — Meses por encima/por debajo del promedio
+
+SELECT 
+    MONTH(fecha_venta) AS MES,
+    SUM(cantidad * precio_unitario) AS TOTAL_MES, --total mensual por cada mes presente en la tabla
+
+    CASE 
+        WHEN SUM(cantidad * precio_unitario) > (
+
+            --calcula promedio general de todos los meses
+            SELECT AVG(total_mensual)
+            FROM (
+                SELECT 
+                    MONTH(fecha_venta) AS mes,
+                    SUM(cantidad * precio_unitario) AS total_mensual
+                FROM ventas
+                GROUP BY MONTH(fecha_venta)
+            ) AS promedio_meses
+
+        ) THEN 'Por encima'
+        ELSE 'Por debajo'
+    END AS RELACION_PROMEDIO
+
+FROM ventas
+GROUP BY MONTH(fecha_venta); -- agrupa por cada mes presente en la tabla (en este caso el mes 3 (marzo)
