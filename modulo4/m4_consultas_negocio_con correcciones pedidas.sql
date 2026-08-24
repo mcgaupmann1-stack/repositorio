a7f7c08e1a0005b2076f242ae07b6ce3dd0f200c
mcgaupmann1-stack <mcgaupmann1@gmail.com>
2026-08-24 14:53:45 -0300

    Add files via upload

---
+++ modulo4/m4_consultas_negocio_con correcciones pedidas.sql
@@ -0,0 +1,67 @@
+
+
+--Consulta 1 — Resumen ejecutivo mensual 
+
+SELECT 
+    MONTH(fecha_venta) AS MES,
+    SUM(cantidad * precio_unitario) AS TOTAL_FACTURADO,
+    COUNT(*) AS CANTIDAD_DE_PEDIDOS,
+    AVG(cantidad * precio_unitario) AS TICKET_PROMEDIO
+FROM 
+    ventas
+GROUP BY 
+    MONTH(fecha_venta);
+
+--Consulta 2 — Ranking de productos
+
+SELECT 
+    id_producto,
+    SUM(cantidad) AS UNIDADES_VENDIDAS,
+    SUM(cantidad * precio_unitario) AS TOTAL_FACTURADO
+FROM 
+    ventas
+GROUP BY 
+    id_producto
+ORDER BY 
+    TOTAL_FACTURADO DESC
+LIMIT 5; --modificado para que funcione en MYSQL
+
+
+--Consulta 3 — Clientes recurrentes
+
+SELECT 
+    id_cliente,
+    COUNT(*) AS CANTIDAD_PEDIDOS,
+    SUM(cantidad * precio_unitario) AS TOTAL_FACTURADO
+FROM 
+    ventas
+GROUP BY id_cliente
+HAVING COUNT(*) > 1;
+
+
+--Consulta 4 — Meses por encima/por debajo del promedio
+
+SELECT 
+    EXTRACT(MONTH FROM fecha_venta) AS MES,  --este cambio no funciona en SQL Server
+    SUM(cantidad * precio_unitario) AS TOTAL_MES, --total mensual por cada mes presente en la tabla
+
+    CASE 
+        WHEN SUM(cantidad * precio_unitario) > (
+
+            --calcula promedio general de todos los meses
+            SELECT AVG(total_mensual)
+            FROM (
+                SELECT 
+                    EXTRACT(MONTH FROM fecha_venta) AS mes,  --este cambio no funciona en SQL Server
+                    SUM(cantidad * precio_unitario) AS total_mensual
+                FROM ventas
+                GROUP BY EXTRACT(MONTH FROM fecha_venta)  --este cambio no funciona en SQL Server
+            ) AS promedio_meses
+
+        ) THEN 'Por encima'
+        ELSE 'Por debajo'
+    END AS RELACION_PROMEDIO
+
+FROM ventas
+GROUP BY EXTRACT(MONTH FROM fecha_venta); -- agrupa por cada mes presente en la tabla (en este caso el mes 3 (marzo)
+--este cambio no funciona en SQL Server
